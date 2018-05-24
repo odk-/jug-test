f86d73c8d4d3576efd42015972470d8e91b7975d
Michal Gryko <odk@odkurzacz.org>
2018-05-24 18:42:08 +0200

    added http server

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Hello JUG!")
+	http.HandleFunc("/hi", hello)
+	http.ListenAndServe(":8888", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello"))
 }
